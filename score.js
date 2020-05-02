7ec83ff38cd51ae5b2cba6de1bbffeee6308bc3a
Yusra <sky.yusra@gmail.com>
2020-05-02 02:27:09 +0100

    Refactor to use arrow functions.

--- score.js
+++ score.js
@@ -9,12 +9,12 @@ papa.parse(file, {
     complete: calculateAndDisplayResults,
 });
 
-function calculateResults(rows) {
+const calculateResults = (rows) => {
     let scores = [];
     const answerRows = rows.data.filter((row, index) => index !== 0);
 
     answerRows.forEach(row => {
-        const [ time, name, ...userAnswers ] = row;
+        const [time, name, ...userAnswers ] = row;
 
         const correctAnswers = userAnswers.reduce((totalCorrect, answer, answerIndex) => {
             const formattedAnswer = convertToComparableString(answer);
@@ -28,33 +28,21 @@ function calculateResults(rows) {
             }
         }, 0);
 
-        return scores.push(
-            {
-                'name': name,
-                'score': correctAnswers
-            }
-        );
+        return scores.push({name, score: correctAnswers});
     });
     return scores;
 }
 
-const convertToComparableString = (answer) => {
-    return answer
-        .replace(/[.,\/#!$%\^&\*’';:{}=\-_`~()]/g, '')
-        .replace(/\s+/g, '')
-        .toLowerCase();
-}
+const convertToComparableString = (answer) => answer
+    .replace(/[.,\/#!$%\^&\*’';:{}=\-_`~()]/g, '')
+    .replace(/\s+/g, '')
+    .toLowerCase();
 
-const sortResults = (scores) => {
-    const sortedScores = scores.sort(function(a, b) {
-        return (b.score - a.score)
-    })
-    console.log(sortedScores);
-}
+const sortedResults = (scores) => scores.sort((a, b) => (b.score - a.score));
 
 function calculateAndDisplayResults(rows) {
     const scores = calculateResults(rows);
-    sortResults(scores);
-
+    const leaderBoard = sortedResults(scores);
+    console.log(leaderBoard);
     return;
 }
